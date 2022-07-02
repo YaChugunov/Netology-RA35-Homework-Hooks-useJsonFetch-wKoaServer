78356f5625d9da57c4602be3a1dd5710588488e7
YaChugunov <informed.looser@gmail.com>
2022-07-02 23:56:29 +0300

    1 file modified

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,46 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + '/app/index.html',
-  filename: 'index.html',
-  inject: 'body',
-});
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const path = require('node:path');
+
+const outputDirectory = 'public';
 
 module.exports = {
-  entry: __dirname + '/app/App.js',
+  entry: ['babel-polyfill', './app/index.js'],
   output: {
-    path: __dirname + '/public',
+    path: path.join(__dirname, outputDirectory),
     filename: 'bundle.js',
   },
-  plugins: [HTMLWebpackPluginConfig],
-  devServer: {
-    inline: true,
-    contentBase: './public',
-    port: 3333,
-  },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel',
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000',
       },
     ],
   },
+  devServer: {
+    port: 3333,
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:3333',
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin([outputDirectory]),
+    new HtmlWebpackPlugin({
+      template: './app/index.html',
+    }),
+  ],
 };
